698312e582366b243751b0feceb983bffd15eb8c
Chaimaa Baaziz <chaimaa@gmail.com>
2019-10-15 15:28:29 +0100

    second commit

---
+++ src/main/java/ma/jit/proxibanque/entities/Client.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ma.jit.proxibanque.entities;
+
+import java.io.Serializable;
+
+/**
+ * @author FOLIO 9740M
+ *
+ */
+public class Client implements Serializable {
+
+	private Long Id;
+	/**
+	 * 
+	 */
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
