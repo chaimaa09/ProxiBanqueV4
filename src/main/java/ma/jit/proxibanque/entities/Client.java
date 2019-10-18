c38c6805d7a3a63ae922c615d8a5775040b93f2e
ayoub <zahraouiayoub1993@gmail.com>
2019-10-18 17:20:21 +0100

    modif port

--- src/main/java/ma/jit/proxibanque/entities/Client.java
+++ src/main/java/ma/jit/proxibanque/entities/Client.java
@@ -24,6 +24,7 @@ import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
@@ -139,6 +140,7 @@ public class Client implements Serializable {
 		this.codePostale = codePostale;
 	}
 
+	@JsonIgnore
 	public List<Compte> getListeComptes() {
 		return listeComptes;
 	}
