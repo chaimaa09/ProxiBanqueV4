ef063721de14d3e7d0d2b8b7b075d08969ccdd0f
Nabila ABRAK <nabila.abrak@gmail.com>
2019-10-18 18:11:37 +0100

    config

--- src/main/java/ma/jit/proxibanque/ProxiBanqueV4Application.java
+++ src/main/java/ma/jit/proxibanque/ProxiBanqueV4Application.java
@@ -3,8 +3,6 @@ package ma.jit.proxibanque;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ma.jit.proxibanque.metier.BanqueMetierImpl;
-import ma.jit.proxibanque.metier.IBanqueMetier;
 
 @SpringBootApplication
 public class ProxiBanqueV4Application {
@@ -13,9 +11,7 @@ public class ProxiBanqueV4Application {
 		SpringApplication.run(ProxiBanqueV4Application.class, args);
 	
 		
-		IBanqueMetier banque = new BanqueMetierImpl();
 		
-		System.out.println(banque.consulterCompte(2L));
 	}
 
 }
