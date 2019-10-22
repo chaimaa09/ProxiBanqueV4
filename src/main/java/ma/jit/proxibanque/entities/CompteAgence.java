f2f284930638b396455558fe25c67310b39b7b3b
Chaimaa Baaziz <chaimaa@gmail.com>
2019-10-22 13:36:07 +0100

    modifier CompteAgence

--- src/main/java/ma/jit/proxibanque/entities/CompteAgence.java
+++ src/main/java/ma/jit/proxibanque/entities/CompteAgence.java
@@ -6,17 +6,36 @@ package ma.jit.proxibanque.entities;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.OneToOne;
+
 /**
  * @author Groupe D
  *
  */
 public class CompteAgence extends Compte {
 
+	@OneToOne
+	private Agence agence;
+	
 	/**
 	 * 
 	 */
 	public CompteAgence() {
 		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the agence
+	 */
+	public Agence getAgence() {
+		return agence;
+	}
+
+	/**
+	 * @param agence the agence to set
+	 */
+	public void setAgence(Agence agence) {
+		this.agence = agence;
 	}
 
 	/**
@@ -26,7 +45,9 @@ public class CompteAgence extends Compte {
 	 */
 	public CompteAgence(Date dateCreation, double solde, List<Operation> operations) {
 		super(dateCreation, solde, operations);
-		// TODO Auto-generated constructor stub
+		
 	}
+	
+	
 
 }
