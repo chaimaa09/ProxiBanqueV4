ea5c06c908fbefb8a039763cc5e1799ea514ed4b
Chaimaa Baaziz <chaimaa@gmail.com>
2019-10-23 03:29:25 +0100

    finaliser les tests

--- src/test/java/ma/jit/proxibanque/ProxiBanqueV4ApplicationTests.java
+++ src/test/java/ma/jit/proxibanque/ProxiBanqueV4ApplicationTests.java
@@ -1,19 +1,15 @@
 package ma.jit.proxibanque;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Date;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -21,8 +17,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import ma.jit.proxibanque.dao.ClientRepository;
 import ma.jit.proxibanque.entities.Client;
-import ma.jit.proxibanque.entities.CompteCourant;
-import ma.jit.proxibanque.entities.Conseiller;
 import ma.jit.proxibanque.metier.IClientMetier;
 
 @RunWith(SpringRunner.class)
@@ -38,22 +32,22 @@ public class ProxiBanqueV4ApplicationTests {
 	@Test
 	public void listeClientsTest() {
 		when(clientRepository.findAll()).thenReturn(Stream
-				.of(new Client(1L, "karama", "basma", "lol", "bk@gmail.com", "casa", "2300", null, null, null),
-						new Client(2L, "karam", "basim", "lol", "bki@gmail.com", "casa", "2300", null, null, null))
+				.of(new Client(1L, "baaziz", "chaimaa", "anfa", "chai@gmail.com", "casa", "2300", null, null, null),
+						new Client(2L, "boo", "rania", "racine", "ran@gmail.com", "casa", "23900", null, null, null))
 				.collect(Collectors.toList()));
 		assertEquals(2, clientMetier.listeClients().size());
 	}
 
 	@Test
 	public void ajouterClientTest() {
-		Client client = new Client(1L, "karama", "basma", "lol", "bk@gmail.com", "casa", "2300");
+		Client client = new Client(1L, "baaziz", "chaimaa", "anfa", "chai@gmail.com", "casa", "2300");
 		when(clientRepository.save(client)).thenReturn(client);
 		assertEquals(client, clientMetier.ajouterClient( client));
 	}
 
 	@Test
 	public void supprimerClientTest() {
-		Client client = new Client(1L,"karama","basma","lol","bk@gmail.com","casa","2300", null, null, null);
+		Client client = new Client(1L, "baaziz", "chaimaa", "anfa", "chai@gmail.com", "casa", "2300", null, null, null);
 		clientMetier.supprimerClient(1L);
 		verify(clientRepository,times(1)).deleteById(1L);
 	}
@@ -61,43 +55,28 @@ public class ProxiBanqueV4ApplicationTests {
 	@Test
 	public void consulterClientTest() {
 		Long id = 1L;
-		Client client = new Client(1L, "karama", "basma", "lol", "bk@gmail.com", "casa", "2300", null, null, null);
+		Client client = new Client(1L, "baaziz", "chaimaa", "anfa", "chai@gmail.com", "casa", "2300", null, null, null);
 		when(clientRepository.getOne(id)).thenReturn(client);
 		assertEquals(client, clientMetier.consulterClient(id));
 
 	}
-//	@Test
-//	public void ajouterClientTest() {
-//	Client client = new Client();
-////	CompteCourant compteCourant = new CompteCourant();
-////	compteCourant.setSolde(500);
-//	client.setNom("Abaach");
-//	client.setPrenom("Mohammed");
-//	client.setEmail("mohamed.abaach@gmail.com");
-//	client.setAdresse("Casa");
-////	client.setCompteCourant(compteCourant);
-////	compteCourant.setClient(client);
-//	Mockito.when(clientRepository.save(client)).thenReturn(client);
-//	assertThat(clientMetier.ajouterClient(client)).isEqualTo(client);
-//	}
-	
+
 	@Test
-	public void ctest() {
-		Client client = new Client();
-		Conseiller conseiller = new Conseiller();
-		CompteCourant compte = new CompteCourant();
-		client.setNom("Abaach");
-		client.setPrenom("Mohammed");
-		client.setEmail("mohamed.abaach@gmail.com");
-		client.setAdresse("Casa");
-		client.setCodePostale("Casa");
-        compte.setSolde(78);
-//        compte.setDateCreation(new Date());
-        client.setCompteCourant(compte);
-        compte.setClient(client);
-        conseiller.getClient().add(client);
-        Mockito.when(clientRepository.save(client)).thenReturn(client);
-        assertThat(clientMetier.ajouterClient(client)).isEqualTo(client);
+	public void updateClientTest() {
+	    Client oldClient = new Client(1L, "baa", "chaima", "maarif", "baa@g;com", "casa", "1223");
+	    when(clientRepository.getOne(1L)).thenReturn(oldClient);
+	    System.out.println(oldClient.toString());
+	    oldClient.setCode(2L);
+	    oldClient.setNom("baaziz");
+	    oldClient.setPrenom("chaimaa");
+	    oldClient.setAdresse("anfa");
+	    oldClient.setEmail("chaimaa@gmail.com");
+	    
+	    Client newClient = oldClient;
+	    System.out.println(oldClient.toString());
+	    
+	    when(clientRepository.save(oldClient)).thenReturn(oldClient);
+	    assertEquals(newClient, clientMetier.ajouterClient(oldClient));
 	}
 	
 }
